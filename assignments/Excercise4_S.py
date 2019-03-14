e6a0adf8ecd607021af2979babc22dc1c289071b
kakakos <kakaarithmatic@gmail.com>
2019-03-14 22:24:51 +0700

    Create Excercise4_S.py

---
+++ assignments/Excercise4_S.py
@@ -0,0 +1,10 @@
+eng = 75.9
+bus = 76.9
+comsys = 77.9
+compro = 78.9
+
+print('--- Your Score ---')
+print('Foundation English : ',eng)
+print('General Business : ',bus)
+print('Introduction to Computer Systems : ',comsys)
+print('Computer Programming : ',compro)
