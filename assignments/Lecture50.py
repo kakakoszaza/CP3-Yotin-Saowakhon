b1df894e4a43ac29e486501901d1dfb1f0a28bd6
kakakos <kakaarithmatic@gmail.com>
2019-06-08 02:55:02 +0700

    lecture 50

---
+++ assignments/Lecture50.py
@@ -0,0 +1,19 @@
+def addnumber(x,y):
+    print('x + y =',x+y)
+
+def engineer(x,y):
+    print('x - y =',x-y)
+
+def kfc(x,y):
+    print('x * y =',x*y)
+
+def avenger(x,y):
+    print('x / y =',x/y)
+
+x = int(input('x = '))
+y = int(input('y = '))
+print('\n')
+addnumber(x,y)
+engineer(x,y)
+kfc(x,y)
+avenger(x,y)
